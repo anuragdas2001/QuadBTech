2eb8da7d4cddb89187f856f1dd30748c5600647d
anuragdas2001 <anuragdas12921@gmail.com>
2024-04-03 13:29:22 +0530

    Initial Commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home-container">
+      <a href="todo" className="btn btn-warning link">
+        To Do App
+      </a>
+    </div>
+  );
+}
+
+export default Home;
